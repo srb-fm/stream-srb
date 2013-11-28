18a3c52b64fa1619bea68e1f8dd8dcd74ea91458
ws22 <technik@srb.fm>
2013-11-28 13:29:33 +0100

    added stream-start.sh

---
+++ stream-start.sh
@@ -0,0 +1,171 @@
+#!/bin/bash
+#
+# This script is for starting darkice-stream and tools
+# to stop darkice and tools use stream-stop.sh
+#
+# Prerequisite is a correct running jack/Qjackctrl
+#
+# Author: Joerg Sorge
+# Distributed under the terms of GNU GPL version 2 or later
+# Copyright (C) Joerg Sorge joergsorge at gmail.com
+# 2013-03-20
+#
+# todo: 
+# check if configfile
+# jamin template
+#
+# config:
+stream_config="stream-set/stream-config.sh"
+
+
+
+## do not edit below this line
+source $stream_config
+
+function f_check_configfile () {
+	if [ ! -f $stream_config ]
+	then
+		message="# Configfile not found..\n$configfile\n Let's let down..."
+		echo $message
+		sleep 5
+		exit
+	fi
+}
+
+
+function f_check_package () {
+        package_install=$1
+        if dpkg-query -s $1 2>/dev/null|grep -q installed; then
+                echo "$package_install installiert"
+        else
+                zenity --error --text="Package:\n$package_install\nnot installt, please install it first!"
+		./stream-stop.sh &
+                exit
+        fi
+}
+
+function f_check_tools_to_use () {
+	ans=$(zenity  --list  --text "Which Tools you want to use?" --checklist  --column "Pick" --column "options" TRUE "Jamin" TRUE "Audiorecorder" FALSE "EBU Meter" FALSE "Meterbridge" --separator=":"); echo $ans
+
+}
+
+function f_start_meterbridge () {
+	message="# Starting Meterbridge..\n"
+	echo $message
+	f_check_package "meterbridge"
+	sleep 1
+	meterbridge -t dpm alsa_pcm:$audio_source_meter_1 alsa_pcm:$audio_source_meter_2 &
+}
+
+function f_start_ebumeter () {
+	message="$message Starting EBU Meter..\n"
+	echo $message
+	f_check_package "ebumeter"
+	sleep 1
+	ebumeter &
+}
+
+function f_check_jack () {
+	message="$message Checking Jack..\n"
+	echo $message
+	sleep 1
+	jack_pid=$(ps aux | grep '[q]jackctl' | awk '{print $2}')
+	if [ "$jack_pid" == "" ]; then
+		zenity --error --text="Qjackctl is not running!\n Please start it befor running this script!\n Let's lay down.."
+		exit		
+	fi
+}
+
+function f_check_jamin () {
+	message="$message Check Jamin..\n"
+	echo $message
+	sleep 1
+	jamin_pid=$(ps aux | grep '[j]amin' | awk '{print $2}')
+	if [ "$jamin_pid" != "" ]; then
+		zenity --error --text="Jamin is alraedy running!\n Please kill it befor using this script.."
+		exit
+	fi
+}
+
+function f_start_jamin () {
+	message="$message Starting Jamin..\n"
+	echo $message
+	f_check_package "jamin"
+	sleep 1
+	jamin &
+}
+
+function f_connect_jamin () {
+	message="$message Connect Jamin..\n"
+	echo $message
+	sleep 2
+	jack_connect $jack_source_1 jamin:in_L &
+	jack_connect $jack_source_2 jamin:in_R &
+}
+
+function f_start_audiorecorder () {
+	message="$message Begin recording..\n"
+	echo $message
+	f_check_package "rotter"
+	sleep 1	
+	rotter -a -f mp3 -b 192 -v -L flat -N SRB_Prot "$path_stream_rec" &
+}
+
+function f_start_stream_init () {
+	message="$message Starting Stream..\n"
+	echo $message
+	f_check_package "darkice"
+	sleep 1
+	cd "$path_stream_tools"
+	pmessage=$(./stream-init.sh start)
+	message="$message\n$pmessage\n"
+	echo $message
+}
+
+function f_connect_darkice () {
+	message="$message Connect Darkice..\n"
+	echo $message
+	sleep 1
+	# pid ermitteln
+	pid_darkice=$(cat $pidfile_int)
+	pdarkice="darkice-$pid_darkice"
+	message="$message $pdarkice"
+	echo $message
+	jack_disconnect $jack_source_1 $pdarkice:left &
+	jack_disconnect $jack_source_2 $pdarkice:right &
+	jack_connect jamin:out_L $pdarkice:left &
+	jack_connect jamin:out_R $pdarkice:right &
+}
+
+
+function f_start_watchdog () {
+	message="$message Starting Watchdog..\n"
+	echo $message
+	sleep 1
+	cd "$path_stream_tools"
+	gnome-terminal -x ./stream-watch.sh start &
+}
+
+
+echo "Starting Stream and Jack-Apps..."
+
+(	echo "10"
+	message="# Starting Tools..\n"
+	f_check_configfile
+	#f_check_tools_to_use
+	f_check_jack
+	f_check_jamin
+	#f_start_meterbridge
+	#f_start_ebumeter
+	f_start_jamin
+	f_connect_jamin
+	#f_start_audiorecorder
+	f_start_stream_init
+	f_connect_darkice
+	f_start_watchdog
+	sleep 5
+	echo "100"
+	
+)| zenity --progress \
+           --title="Stream" --text="starten..." --width=500 --pulsate --auto-close
+
